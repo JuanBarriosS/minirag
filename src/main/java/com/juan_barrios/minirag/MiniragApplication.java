30fbaa7993cadb2a2440e162f2dc927c062f1971
Juan Camilo Barrios <barriossaltarinj@gmail.com>
2026-09-15 14:58:40 -0500

    comit

---
+++ src/main/java/com/juan_barrios/minirag/MiniragApplication.java
@@ -0,0 +1,13 @@
+package com.juan_barrios.minirag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniragApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniragApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juan_barrios/minirag/MiniragApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan_barrios.minirag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniragApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
