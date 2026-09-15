bab6782faf23e11a9dd638795d8d6744df4a7f24
Juan Camilo Barrios <barriossaltarinj@gmail.com>
2026-09-15 16:25:56 -0500

    update

--- src/main/java/com/juan_barrios/minirag/service/ChatService.java
+++ src/main/java/com/juan_barrios/minirag/service/ChatService.java
@@ -1,5 +1,71 @@
 package com.juan_barrios.minirag.service;
 
+import com.juan_barrios.minirag.model.Consulta;
+import com.juan_barrios.minirag.repository.ConsultaRepository;
+import org.springframework.ai.chat.client.ChatClient;
+import
+org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ChatService {
-    
+ private final ChatClient chatClient;
+ private final ConsultaRepository consultaRepository;
+ public ChatService(
+ ChatClient.Builder builder,
+ VectorStore vectorStore,
+ ConsultaRepository consultaRepository) {
+ this.consultaRepository =
+ consultaRepository;
+ SearchRequest searchRequest =
+ SearchRequest.builder()
+ .topK(4)
+ .similarityThreshold(0.50)
+ .build();
+ QuestionAnswerAdvisor ragAdvisor =
+ QuestionAnswerAdvisor
+ .builder(vectorStore)
+ .searchRequest(searchRequest)
+ .build();
+ this.chatClient =
+ builder
+ .defaultSystem("""
+ Eres un asistente académico.
+ Responde utilizando exclusivamente
+ la información recuperada de la
+ base documental.
+ Si la información necesaria no está
+ disponible en el contexto proporcionado,
+ responde exactamente:
+ "No encuentro esa información en los
+ documentos disponibles."
+ No inventes información.
+ Responde de manera clara, breve y
+ académicamente correcta.
+ """)
+ .defaultAdvisors(
+ ragAdvisor
+ )
+ .build();
+ }
+ public String preguntar(
+ String pregunta) {
+ String respuesta =
+ chatClient
+ .prompt()
+ .user(pregunta)
+ .call()
+ .content();
+ Consulta consulta =
+ new Consulta(
+ pregunta,
+ respuesta
+ );
+ consultaRepository.save(
+ consulta
+ );
+ return respuesta;
+ }
 }
